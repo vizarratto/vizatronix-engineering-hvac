66495a3fba40c9c5ef9151dc1e11f48d29b184ff
vizarratto <102439336+vizarratto@users.noreply.github.com>
2026-05-16 00:37:53 +0200

    Update vizatronix_app.py

--- vizatronix_app.py
+++ vizatronix_app.py
@@ -9,7 +9,7 @@ import os
 import base64
 
 # --- CONFIGURAZIONE PAGINA ---
-st.set_page_config(page_title="Vizatronix Engineering PRO Pino Mazzitelli", page_icon="🏗️", layout="wide")
+st.set_page_config(page_title="Vizatronix Engineering PRO", page_icon="🏗️", layout="wide")
 
 # --- CUSTOM CSS PER MOBILE ---
 st.markdown("""
